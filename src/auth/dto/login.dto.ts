29cd65340b55ad22cb74e34b0b953a34ab4e5a5d
ARANO8 <alanarnez51@gmail.com>
2026-01-10 21:15:54 -0400

    feat: ✨️ Integrar swagger para la documentacion API

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,20 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({
+    example: 'm.teran@aceaa.org',
+    description: 'Correo corporativo',
+  })
   @IsEmail({}, { message: 'El correo electrónico no es válido' })
   @IsNotEmpty({ message: 'El correo electrónico es obligatorio' })
   email: string;
 
+  @ApiProperty({
+    example: '123456',
+    minLength: 6,
+    description: 'Contraseña del usuario',
+  })
   @IsNotEmpty({ message: 'La contraseña es obligatoria' })
   @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
   password: string;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,18 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('SIFIN API - Conservación Amazónica')
+    .setDescription(
+      'API para el Sistema Financiero Integrado (POA, Solicitudes, Rendiciones)',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
