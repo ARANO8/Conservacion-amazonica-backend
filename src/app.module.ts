7a7ce73d8aa0a02cc8b878941d937b7e4cbac4ed
ARANO8 <alanarnez51@gmail.com>
2026-01-15 17:41:44 -0400

    chore: 🔧  remove legacy catalogs and requests modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-import { RequestsModule } from './requests/requests.module';
-import { CatalogsModule } from './catalogs/catalogs.module';
-
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    AuthModule,
-    RequestsModule,
-    CatalogsModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
 })
 export class AppModule {}
